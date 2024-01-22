dc708df17f8fed071a610a65f63ea3bcfdb819d1
Huy Hoang <hoangndhhe171375@fpt.edu.vn>
2024-01-22 22:36:11 +0700

    hoang

--- WebClient/Controllers/HomeController.cs
+++ WebClient/Controllers/HomeController.cs
@@ -105,5 +105,9 @@ namespace WebClient.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
     }
 }
