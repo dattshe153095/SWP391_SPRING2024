69c0b9c7b7a98c6d6d1864cfde9b6dae511a5467
chien nguyen <chiennnhe173242@fpt.edu.vn>
2024-01-24 22:17:58 +0700

    update profile

--- WebClient/Controllers/AccountController.cs
+++ WebClient/Controllers/AccountController.cs
@@ -1,14 +1,19 @@
 ﻿using BussinessObject.Models;
+using DataAccess.Captcha;
 using DataAccess.DAO;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Text;
 using System.Xml.Linq;
 
 namespace WebClient.Controllers
 {
     public class AccountController : Controller
     {
+        private string captchaCode="";
         public IActionResult Profile()
         {
             ViewBag.accountId = HttpContext.Session.GetInt32("Account");
@@ -32,10 +37,41 @@ namespace WebClient.Controllers
             }
             return RedirectToAction("Profile", "Account");
         }
-
-        [HttpPost]
-        public IActionResult ChangePassword(string new_password, string confirmPassword)
+        
+        /*[HttpGet]
+        
+        public IActionResult ChangePassword()
         {
+            
+            Captcha oCaptcha = new Captcha();
+            Random rnd = new Random();
+            string[] s = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+            int i;
+            StringBuilder sb = new StringBuilder(4);
+            for (i = 0; i <= 4; i++)
+            {
+                sb.Append(s[rnd.Next(1, s.Length)]);
+            }
+            Bitmap bm = oCaptcha.MakeCaptchaImage(sb.ToString(), 200, 100, "Arial");
+            captchaCode = bm.ToString();
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bm.Save(ms, ImageFormat.Png);
+                byte[] imageBytes = ms.ToArray();
+                ViewBag.CaptchaImageBytes = Convert.ToBase64String(imageBytes);
+            }
+
+
+
+            return RedirectToAction("Profile", "Account");
+        }*/
+        
+        [HttpPost]
+        
+        public IActionResult ChangePassword(string new_password, string confirmPassword, string captcha)
+        {
+            
             if (HttpContext.Session.GetInt32("Account") != null)
             {
                 int? id = HttpContext.Session.GetInt32("Account");
@@ -50,5 +86,7 @@ namespace WebClient.Controllers
             }
             return RedirectToAction("Profile", "Account");
         }
+        
+       
     }
 }
