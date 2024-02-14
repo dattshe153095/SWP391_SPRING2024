942107a53aaf459de5bc83193cdcfd2d3d21ba3d
dattshe153095 <dattshe153095@fpt.edu.vn>
2024-02-14 19:41:17 +0700

    push Wallet list

---
+++ DataAccess/DAO/WalletDAO.cs
@@ -0,0 +1,37 @@
+﻿using BussinessObject.Models;
+using BussinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAO
+{
+    public class WalletDAO
+    {
+        public static List<Wallet> GetAllWallet()
+        {
+            List<Wallet> list = new List<Wallet>();
+
+            using (var context = new Web_Trung_GianContext())
+            {
+                list = context.Wallets.ToList();
+
+            }
+            return list;
+        }
+
+        public static Wallet GetWalletById(int id)
+        {
+            Wallet wallet = new Wallet();
+
+            using (var context = new Web_Trung_GianContext())
+            {
+                wallet = context.Wallets.ToList().FirstOrDefault(x => x.id == id);
+
+            }
+            return wallet;
+        }
+    }
+}
---
+++ WebClient/Controllers/WalletController.cs
@@ -0,0 +1,33 @@
+﻿using BussinessObject.Models;
+using DataAccess.DAO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class WalletController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult WithdrawalTransaction()
+        {
+            return View();
+        }
+
+        public ActionResult DepositTransaction()
+        {
+            List<Wallet> wallets = new List<Wallet>();
+            wallets = WalletDAO.GetAllWallet();
+            ViewBag.Wallets = wallets;
+            return View();
+        }
+
+        public ActionResult ReportTransaction()
+        {
+            return View();
+        }
+    }
+}
