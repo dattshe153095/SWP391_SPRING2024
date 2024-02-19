2cde750f3dd98aaf6cdb24e8ee297d713f4edea7
dattshe153095 <dattshe153095@fpt.edu.vn>
2024-02-19 12:37:44 +0700

    fix product

---
+++ WebClient/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using BussinessObject.Models;
+using DataAccess.Captcha;
+using DataAccess.DAO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Text;
+using System.Xml.Linq;
+
+namespace WebClient.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = ProductDAO.GetAllProduct();
+            ViewBag.Products = products;
+            return View();
+        }
+
+    }
+}
