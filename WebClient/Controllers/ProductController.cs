9ea0877aa84cdd35bb3fc41a5d5e57f06eca17ed
dattshe153095 <dattshe153095@fpt.edu.vn>
2024-02-19 13:23:01 +0700

    add project

---
+++ WebClient/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using BussinessObject.Models;
+using DataAccess.Captcha;
+using DataAccess.DAO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Text;
+using System.Xml.Linq;
+
+namespace WebClient.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = ProductDAO.GetAllProduct();
+            ViewBag.Products = products;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetProductDetail(int productId)
+        {
+            var product = ProductDAO.GetProductWithId(productId);
+
+            if (product != null)
+            {
+                return Json(product);
+            }
+            else
+            {
+                return Json(new { message = "Không tìm thấy thông tin sản phẩm." });
+            }
+        }
+
+    }
+}
