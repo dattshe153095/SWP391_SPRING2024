a55bdc1baf338e170da5081ad1711412e5e27a3d
dattshe153095 <dattshe153095@fpt.edu.vn>
2024-02-19 12:55:41 +0700

    fix product

--- WebClient/Controllers/ProductController.cs
+++ WebClient/Controllers/ProductController.cs
@@ -20,5 +20,20 @@ namespace WebClient.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult GetProductDetail(int productId)
+        {
+            var product = ProductDAO.GetProductWithId(productId);
+
+            if (product != null)
+            {
+                return Json(product);
+            }
+            else
+            {
+                return Json(new { message = "Không tìm thấy thông tin sản phẩm." });
+            }
+        }
+
     }
 }
