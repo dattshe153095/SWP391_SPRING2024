5bdfe147e64b8b2fa1a79ab6ab61ae2d2994847f
Huy Hoang <hoangndhhe171375@fpt.edu.vn>
2024-02-21 22:44:21 +0700

    hoang

---
+++ WebClient/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BussinessObject.Models;
+using DataAccess.Captcha;
+using DataAccess.DAO;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Text;
+using System.Xml.Linq;
+
+namespace WebClient.Controllers
+{
+    public class HistoryOrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = ProductDAO.GetAllProduct();
+            ViewBag.Products = products;
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult GetProductDetail(int productId)
+        {
+            var product = ProductDAO.GetProductWithId(productId);
+
+            if (product != null)
+            {
+                return Json(product);
+            }
+            else
+            {
+                return Json(new { message = "Không tìm thấy thông tin sản phẩm." });
+            }
+        }
+    }
+}
