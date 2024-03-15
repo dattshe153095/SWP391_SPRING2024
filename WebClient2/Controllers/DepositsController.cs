005a0c3ccd4994df1e005b38d2abf84da4bc19cc
dattshe153095 <dattshe153095@fpt.edu.vn>
2024-03-15 23:59:54 +0700

    Update DepositsController.cs

--- WebClient2/Controllers/DepositsController.cs
+++ WebClient2/Controllers/DepositsController.cs
@@ -64,12 +64,12 @@ namespace WebClient2.Controllers
 
         public IActionResult PaymentFail()
         {
-            return View("Payment Fail");
+            return View();
         }
 
         public IActionResult PaymentSuccess()
         {
-            return View("Payment Success");
+            return View();
         }
 
         public IActionResult PaymentCallBack()
