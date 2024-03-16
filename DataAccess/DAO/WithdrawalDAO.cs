dbc9d4442d3691c6af0bab258f62d210f400b017
dattshe153095 <dattshe153095@fpt.edu.vn>
2024-03-16 22:13:55 +0700

    push rut tien

--- DataAccess/DAO/WithdrawalDAO.cs
+++ DataAccess/DAO/WithdrawalDAO.cs
@@ -12,62 +12,19 @@ namespace DataAccess.DAO
 {
     public class WithdrawalDAO
     {
-        private static Queue<Withdrawal> withdrawalQueue = new Queue<Withdrawal>();
-        private static object queueLock = new object();
-        private static bool isProcessing = false;
 
         public static void CreateWithdrawal(Withdrawal withdrawal)
         {
-            withdrawal.status = StatusEnum.DANG_XU_LI;
+            withdrawal.status = StatusEnum.DANG_CHO_XAC_NHAN;
+            withdrawal.state = StateEnum.DANG_XU_LI;
             withdrawal.create_at = DateTime.Now;
             withdrawal.update_at = DateTime.Now;
 
-            lock (queueLock)
+            using (var context = new Web_Trung_GianContext())
             {
-                withdrawalQueue.Enqueue(withdrawal); // Thêm deposit vào hàng đợi
-                if (!isProcessing)
-                {
-                    isProcessing = true;
-                    ProcessWithdrawals(); // Bắt đầu xử lý hàng đợi nếu chưa có công việc nào đang được xử lý
-                }
-            }
-        }
-
-        private static void ProcessWithdrawals()
-        {
-            while (true)
-            {
-                Withdrawal withdrawal;
-                lock (queueLock)
-                {
-                    if (withdrawalQueue.Count == 0)
-                    {
-                        isProcessing = false;
-                        return; // Kết thúc nếu không còn deposit nào trong hàng đợi
-                    }
-                    withdrawal = withdrawalQueue.Dequeue(); // Lấy deposit đầu tiên từ hàng đợi
-                }
-
-                try
-                {
-                    if (withdrawal.status == StatusEnum.DANG_XU_LI)
-                    {
-                        withdrawal.status = StatusEnum.DANG_CHO_XAC_NHAN;
-                        WalletDAO.UpdateWalletWithdrawalBalance(withdrawal.wallet_id, withdrawal.amount);
-                        using (var context = new Web_Trung_GianContext())
-                        {
-                            var withdrawals = context.Set<Withdrawal>();
-                            withdrawals.Add(withdrawal);
-                            context.SaveChanges();
-                        }
-                    }
-
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Error creating withdrawal: " + ex.Message);
-                    // Xử lý lỗi tạo deposit ở đây nếu cần thiết
-                }
+                var deposits = context.Set<Withdrawal>();
+                deposits.Add(withdrawal);
+                context.SaveChanges();
             }
         }
 
---
+++ WebClient2/ViewModel/WithdrawalModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebClient2.ViewModel
+{
+    public class WithdrawalModel
+    {
+        [Range(20000, 500000, ErrorMessage = "Số tiền chỉ trong khoảng từ 20,000 đến 500,000")]
+        public int amount { get; set; }
+        [MaxLength(50, ErrorMessage="Số kí tự dưới 50 kí hiệu")]
+        public string bank_number { get; set; }
+        [MaxLength(50, ErrorMessage = "Số kí tự dưới 50 kí hiệu")]
+        public string bank_user { get; set; }
+        [MaxLength(100, ErrorMessage = "Số kí tự dưới 50 kí hiệu")]
+        public string bank_name { get; set; }
+    }
+}
