753ee5643dea4db3fcfe1184b6154fa999c65973
hungnphe173547 <hungnphe173547@fpt.edu.vn>
2024-03-28 22:09:09 +0700

    ad

--- WebClient/Controllers/AdminController.cs
+++ WebClient/Controllers/AdminController.cs
@@ -11,6 +11,7 @@ using System.Drawing.Imaging;
 using System.Drawing;
 using System.Text;
 using DataAccess.ViewModel;
+using DataAccess.Library;
 
 namespace WebClient2.Controllers
 {
@@ -44,7 +45,7 @@ namespace WebClient2.Controllers
             IntermediateOrder order = IntermediateOrderDAO.GetIntermediateOrderById(id);
             OrderViewModel orderView = new OrderViewModel();
 
-            OrderViewModel orderViewModel= IntermediateOrderDAO.GetOrderViewModel(order);
+            OrderViewModel orderViewModel = IntermediateOrderDAO.GetOrderViewModel(order);
             return View(orderView);
 
         }
@@ -58,7 +59,64 @@ namespace WebClient2.Controllers
             List<Deposit> deposits = DepositDAO.GetAllDeposit();
             return View(deposits);
         }
+        public IActionResult DeleteInterOrder(string? id)
+        {
+            int account_id = HttpContext.Session.GetInt32("Account").Value;
+
+
+            IntermediateOrder order = IntermediateOrderDAO.GetIntermediateOrderById(id);
+            if (order.status == IntermediateOrderEnum.YEU_CAU_QUAN_TRI)
+            {
+                if (order.buy_user != null)
+                {
+                    WalletDAO.UpdateWalletDepositBalance(order.buy_user.Value, (int)order.payment_amount);
+                }
+                IntermediateOrderDAO.DeleteInterOrder(id);
+                TempData["Message"] = "Hủy đơn hàng thành công trả tiền cho người mua";
+                return RedirectToAction("DetailOrder", new { id = id });
+            }
+            return RedirectToAction("DetailOrder", new { id = id });
+        }
+        #region WITHDRAWAL
+        public IActionResult WithdrawalManage()
+        {
+            List<Withdrawal> withdrawals = WithdrawalDAO.GetAllWithdrawal();
+            return View(withdrawals);
+        }
+        public IActionResult DetailWithdrawal(string? id)
+        {
+            Withdrawal withdrawal = WithdrawalDAO.GetWithdrawalById(id);
+            if (withdrawal != null)
+            {
+                return View(withdrawal);
+            }
+            return RedirectToAction("WithdrawalManage");
+
+        }
+        public IActionResult CompleteWithdrawal(string? id)
+        {
+            Withdrawal withdrawal = WithdrawalDAO.GetWithdrawalById(id);
+            if (withdrawal == null)
+            {
+                return RedirectToAction("WithdrawalManage");
+            }
+
+
+            if (withdrawal.state == StateEnum.DANG_XU_LI)
+            {
+                withdrawal.state = StateEnum.THANH_CONG;
+                withdrawal.status = StatusEnum.HOAN_THANH;
+                withdrawal.update_by = HttpContext.Session.GetInt32("Account").Value;
+                WithdrawalDAO.UpdateWithdrawal(withdrawal);
+                TempData["Message"] = "Cập nhật trạng thái đơn rút sang thành công";
+            }
+            else
+            {
+                TempData["Message"] = "Đơn đã xử lí trước đó, không thể cập nhật";
+            }
+            return RedirectToAction("DetailWithdrawal", new { id = withdrawal.id });
+
+        }
+        #endregion
     }
-
-
 }
